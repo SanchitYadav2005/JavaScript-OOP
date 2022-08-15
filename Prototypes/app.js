1c72d5b119a15fa18acc960c2eb23190d08eccc1
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-15 08:17:36 +0530

    we can change assined prototype

--- Prototypes/app.js
+++ Prototypes/app.js
@@ -3,3 +3,9 @@ String.prototype.yell = function() { // we assined a new funciton apart form fun
 }
 
 console.log("we won".yell())
+
+Array.prototype.pop = function(){ // we can also change what already assined funciton do.
+    return `hello array`
+}
+
+console.log([0,1].pop())
